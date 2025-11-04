8f61479e60611629e8924c40f6db6bdfd196699e
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-11-04 21:49:34 +0000

    Add basic Playwright tests

---
+++ test/basic.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "@playwright/test";
+
+test("toast is rendered and then disappears", async ({ page }) => {
+  page.goto("http://localhost:5173");
+
+  await page.click("button");
+
+  await expect(page.locator("[data-sonner-toast]")).toHaveCount(1);
+  await expect(page.locator("[data-sonner-toast]")).toHaveCount(0);
+});
