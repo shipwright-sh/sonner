f048b7c24fa6e9322388d04658013b717711c167
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-11-05 12:27:46 +0000

    Add dts Vite plugin

--- packages/solid/CHANGELOG.md
+++ packages/solid/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @shipwright-sh/sonner-solid
 
+## 0.1.1
+
+### Patch Changes
+
+- Add type definitions
+
 ## 0.1.0
 
 ### Minor Changes
--- packages/solid/vite.config.ts
+++ packages/solid/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.tsx"),
