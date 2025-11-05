faf4c06db4497c6df72c2ca7430adf185fcd5810
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-11-05 09:53:41 +0000

    Extract core functionality into the core package

---
+++ packages/core/types.ts
@@ -0,0 +1,43 @@
+export type ToastTypes =
+  | "normal"
+  | "action"
+  | "success"
+  | "info"
+  | "warning"
+  | "error"
+  | "loading"
+  | "default";
+
+export type Position =
+  | "top-left"
+  | "top-right"
+  | "bottom-left"
+  | "bottom-right"
+  | "top-center"
+  | "bottom-center";
+
+export type SwipeDirection = "top" | "right" | "bottom" | "left";
+
+export type Theme = "light" | "dark";
+
+export interface HeightT {
+  height: number;
+  toastId: number | string;
+  position: Position;
+}
+
+export enum SwipeStateTypes {
+  SwipedOut = "SwipedOut",
+  SwipedBack = "SwipedBack",
+  NotSwiped = "NotSwiped",
+}
+
+export type Offset =
+  | {
+      top?: string | number;
+      right?: string | number;
+      bottom?: string | number;
+      left?: string | number;
+    }
+  | string
+  | number;
